1c50325ec681faeb480294eaab32023362ea15af
tomclemente <tomangelo@gmail.com>
2020-09-12 04:20:37 +0800

    Added Contact API

---
+++ contact-api/app.js
@@ -0,0 +1,65 @@
+var AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+       
+    let body;
+    let statusCode = '200';
+    let params = JSON.parse(event["body"]); 
+
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    try {    
+        body = await new Promise((resolve, reject) => {
+            var emailParam = generateEmailparam(params);
+            sendEmail(emailParam).then(resolve,reject);
+ 
+        });
+
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
+
+function sendEmail(params) {
+    return new Promise((resolve, reject) => {
+        var ses = new AWS.SES({region: 'us-east-1'});
+        ses.sendEmail(params, function (err, data) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                console.log(data);
+                resolve(data);
+            }
+        });
+    });
+};
+
+function generateEmailparam(params) {
+    var param = {
+        Destination: {
+            ToAddresses: ["contact@ratingsuite.com"]
+        },
+        Message: {
+            Body: {
+                Text: { Data: params.body
+
+                }
+            },
+            Subject: { Data: params.name + " : " + params.subject + " : " + params.email}
+        },
+        Source: "bb452@cornell.edu"
+    };
+    return param;
+}
