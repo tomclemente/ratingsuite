16fcf6a0de9339ee0dfbb9e742ed4ac7120c3134
tomclemente <underscor_e@yahoo.com>
2020-11-16 05:20:13 +0800

    updated allowed headers

--- review-api/app.js
+++ review-api/app.js
@@ -45,7 +45,7 @@ exports.handler = async (event, context) => {
 
     const headers = {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE"
     };
