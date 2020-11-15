5b9e555814ee45413ebd51162b8a1a580521b114
tomclemente <underscor_e@yahoo.com>
2020-11-16 05:23:05 +0800

    updated allowed headers

--- notification-api/app.js
+++ notification-api/app.js
@@ -33,7 +33,7 @@ exports.handler = async (event) => {
 
     const headers = {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE"
     };
